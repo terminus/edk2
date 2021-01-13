ca79a4f0eea47577c6a54dc6f0d5d8f9411ce91c
Ankur Arora <ankur.a.arora@oracle.com>
2021-01-13 00:36:56 +0000

    UefiCpuPkg: add CPU ejection support
    Define CPU_HOT_EJECT_DATA and add PCD PcdCpuHotEjectDataAddress,
    which would be used to share CPU ejection state between
    PiSmmCpuDxeSmm and OvmfPkg/CpuHotPlugSmm.
    
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Cc: Boris Ostrovsky <boris.ostrovsky@oracle.com>
    Cc: Aaron Young <aaron.young@oracle.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=3132
    Signed-off-by: Ankur Arora <ankur.a.arora@oracle.com>

--- UefiCpuPkg/Include/CpuHotPlugData.h
+++ UefiCpuPkg/Include/CpuHotPlugData.h
@@ -2,6 +2,7 @@
 Definition for a structure sharing information for CPU hot plug.
 
 Copyright (c) 2013 - 2015, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2021, Oracle Corporation.<BR>
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
@@ -23,5 +24,25 @@ typedef struct {
   UINT32    SmrrBase;
   UINT32    SmrrSize;
 } CPU_HOT_PLUG_DATA;
+
+typedef
+VOID
+(EFIAPI *CPU_HOT_EJECT_FN)(
+  IN UINTN  ProcessorNum
+  );
+
+#define CPU_EJECT_INVALID	(MAX_UINT64)
+#define CPU_EJECT_WORKER	(MAX_UINT64-1)
+
+#define  CPU_HOT_EJECT_DATA_REVISION_1         0x00000001
+
+typedef struct {
+  UINT32           Revision;          // Used for version identification for this structure
+  UINT32           ArrayLength;       // The entries number of the following ApicId array and SmBase array
+
+  UINT64           *ApicIdMap;        // Pointer to CpuIndex->ApicId map. Holds APIC IDs for pending ejects
+  CPU_HOT_EJECT_FN Handler;           // Handler for CPU ejection
+  UINT64           Reserved;
+} CPU_HOT_EJECT_DATA;
 
 #endif
